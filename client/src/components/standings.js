fae3b1081ef3cc7c047b50bdca44464605d35ecc
jkap86 <jkap86@gmail.com>
2022-06-08 12:03:40 -0400

    -

--- client/src/components/standings.js
+++ client/src/components/standings.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Roster from "./roster";
 import volcano from '../volcano.png';
 import Search from "./search";
@@ -6,8 +6,33 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const Standings = (props) => {
     const [leagues, setLeagues] = useState([])
-    if (props.leagues !== leagues) setLeagues(props.leagues)
 
+
+    useEffect(() => {
+        setLeagues(props.leagues.sort((a, b) => b.wins - a.wins || b.fpts - a.fpts))
+    }, [props.leagues])
+
+    const sort = (sort_by) => {
+        let l = leagues
+        switch (sort_by) {
+            case 'fpts':
+                setLeagues([...l.sort((a, b) => b.fpts - a.fpts)])
+                break;
+            case 'record':
+                setLeagues([...l.sort((a, b) => b.wins - a.wins || a.losses - b.losses || b.fpts - a.fpts)])
+                break;
+            case 'value':
+                setLeagues([...l.sort((a, b) =>
+                    b.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0) -
+                    a.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0)
+                )])
+                break;
+            case 'fpts_against':
+                setLeagues([...l.sort((a, b) => b.fpts_against - a.fpts_against)])
+                break;
+        }
+    }
+    
     const getSearched = (data) => {
         const l = leagues
         if (data) {
@@ -38,14 +63,14 @@ const Standings = (props) => {
                     <th colSpan={3}>Username</th>
                     <th></th>
                     <th colSpan={4}>League</th>
-                    <th colSpan={2}>Record</th>
-                    <th colSpan={2}>FP</th>
-                    <th colSpan={2}>FPA</th>
-                    <th colSpan={2}>Roster Value</th>
+                    <th colSpan={2} onClick={() => sort('record')} className="clickable">Record</th>
+                    <th colSpan={2} onClick={() => sort('fpts')} className="clickable">FP</th>
+                    <th colSpan={2} onClick={() => sort('fpts_against')} className="clickable">FPA</th>
+                    <th colSpan={2} onClick={() => sort('value')} className="clickable">Roster Value</th>
                 </tr>
             </tbody>
-            {leagues.filter(x => x.isLeagueHidden === false).sort((a, b) => b.wins - a.wins || b.fpts - a.fpts || b.fpts_against - a.fpts_against || b.owner_id - a.owner_id).map((league, index) =>
-                <AnimatePresence exitBeforeEnter>
+            {leagues.filter(x => x.isLeagueHidden === false).map((league, index) =>
+                <AnimatePresence exitBeforeEnter key={index}>
                     <motion.tbody
                         key={league.owner_id}
                         initial={{ y: 900 }}
@@ -54,7 +79,7 @@ const Standings = (props) => {
                         transition={{ duration: 1, type: "spring" }}
                         className={league.isRosterHidden === true ? 'hover clickable' : 'active clickable'}
                     >
-                        <motion.tr
+                        <tr
                             className={league.isRosterHidden === true ? 'hover clickable' : 'active clickable'}
                             onClick={() => props.showRoster(league.owner_id)}
                         >
@@ -82,7 +107,7 @@ const Standings = (props) => {
                             <td colSpan={2}>{league.fpts.toLocaleString("en-US")}</td>
                             <td colSpan={2}>{league.fpts_against.toLocaleString("en-US")}</td>
                             <td colSpan={2}>{league.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0).toLocaleString("en-US")}</td>
-                        </motion.tr>
+                        </tr>
                         {league.isRosterHidden === true ? null :
                             <AnimatePresence>
                                 <motion.tr
