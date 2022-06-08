0f7c576bd4f3519a885531de52322fc86139e6b5
jkap86 <jkap86@gmail.com>
2022-06-08 13:52:05 -0400

    -

--- client/src/components/standings.js
+++ client/src/components/standings.js
@@ -3,17 +3,44 @@ import Roster from "./roster";
 import volcano from '../volcano.png';
 import Search from "./search";
 import { motion, AnimatePresence } from "framer-motion";
+import allPlayers from '../allPlayers.json';
 
 const Standings = (props) => {
     const [leagues, setLeagues] = useState([])
-
-
+    const [group_age, setGroup_age] = useState("All")
+    console.log(props.leagues)
     useEffect(() => {
         setLeagues(props.leagues.sort((a, b) => b.wins - a.wins || b.fpts - a.fpts ||
             b.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0) -
             a.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0)
         ))
     }, [props.leagues])
+
+    const getAge = (username) => {
+        let l = leagues
+        l = l.find(x => x.username === username)
+        let length;
+        switch (group_age) {
+            case "All":
+                length = l.players.length
+                l = l.players.reduce((acc, cur) => acc + allPlayers[cur].age, 0)
+                break;
+            case "Starters":
+                length = l.players.filter(x => l.starters.includes(x)).length
+                l = l.players.filter(x => l.starters.includes(x)).reduce((acc, cur) => acc + allPlayers[cur].age, 0)
+                break;
+            case "Bench":
+                length = l.players.filter(x => !l.starters.includes(x)).length
+                l = l.players.filter(x => !l.starters.includes(x)).reduce((acc, cur) => acc + allPlayers[cur].age, 0)
+                break;
+            default:
+                length = l.players.filter(x => allPlayers[x].position === group_age).length
+                l = l = l.players.filter(x => allPlayers[x].position === group_age).reduce((acc, cur) => acc + allPlayers[cur].age, 0)
+                break;
+        }
+
+        return (l / length).toFixed(2)
+    }
 
     const sort = (sort_by) => {
         let l = leagues
@@ -29,6 +56,9 @@ const Standings = (props) => {
                     b.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0) -
                     a.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0)
                 )])
+                break;
+            case 'age':
+                setLeagues([...l.sort((a, b) => getAge(a.username) - getAge(b.username))])
                 break;
             case 'fpts_against':
                 setLeagues([...l.sort((a, b) => b.fpts_against - a.fpts_against)])
@@ -70,6 +100,18 @@ const Standings = (props) => {
                     <th colSpan={2} onClick={() => sort('fpts')} className="clickable">FP</th>
                     <th colSpan={2} onClick={() => sort('fpts_against')} className="clickable">FPA</th>
                     <th colSpan={2} onClick={() => sort('value')} className="clickable">Roster Value</th>
+                    <th colSpan={2} onClick={() => sort('age')} className="clickable">
+                        <select onChange={(e) => setGroup_age(e.target.value)}>
+                            <option>All</option>
+                            <option>Starters</option>
+                            <option>Bench</option>
+                            <option>QB</option>
+                            <option>RB</option>
+                            <option>WR</option>
+                            <option>TE</option>
+                        </select>
+                        <p>Avg Age</p>
+                    </th>
                 </tr>
             </tbody>
             {leagues.filter(x => x.isLeagueHidden === false).map((league, index) =>
@@ -110,6 +152,7 @@ const Standings = (props) => {
                             <td colSpan={2}>{league.fpts.toLocaleString("en-US")}</td>
                             <td colSpan={2}>{league.fpts_against.toLocaleString("en-US")}</td>
                             <td colSpan={2}>{league.players.reduce((acc, cur) => acc + parseInt(props.matchPlayer(cur)), 0).toLocaleString("en-US")}</td>
+                            <td colSpan={2}>{getAge(league.username)}</td>
                         </tr>
                         {league.isRosterHidden === true ? null :
                             <AnimatePresence>
@@ -121,7 +164,7 @@ const Standings = (props) => {
                                     transition={{ duration: 1, type: "spring" }}
                                     className='expanded'
                                 >
-                                    <td colSpan={17}>
+                                    <td colSpan={19}>
                                         <Roster roster={league} matchPlayer={props.matchPlayer} matchPick={props.matchPick} />
                                     </td>
                                 </motion.tr>
